05fb5bf80cdcc807900971fcc153732f27565500
Rob Decker <rob@deckerdev.com>
2018-10-10 16:32:52 -0700

    Update installer

---
+++ Brewfile
@@ -0,0 +1,185 @@
+tap "buo/cask-upgrade"
+tap "homebrew/apache"
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/cask-fonts"
+tap "homebrew/cask-versions"
+tap "homebrew/command-not-found"
+tap "homebrew/completions"
+tap "homebrew/core"
+tap "homebrew/dupes"
+tap "homebrew/php"
+tap "homebrew/services"
+tap "homebrew/versions"
+cask "xquartz"
+brew "ack"
+brew "sqlite"
+brew "ansible"
+brew "autoconf"
+brew "automake"
+brew "automysqlbackup"
+brew "brew-cask-completion"
+brew "coreutils"
+brew "diff-so-fancy"
+brew "gnupg"
+brew "duplicity"
+brew "duply"
+brew "fish"
+brew "freetype"
+brew "fontconfig"
+brew "gawk"
+brew "git"
+brew "git-extras"
+brew "handbrake"
+brew "hicolor-icon-theme"
+brew "hostess"
+brew "httpd"
+brew "libtool"
+brew "imagemagick"
+brew "jq"
+brew "libdvdcss"
+brew "mackup"
+brew "mariadb"
+brew "mas"
+brew "mhash"
+brew "mcrypt"
+brew "md5sha1sum"
+brew "node@8"
+brew "php@7.1", link: true
+brew "pv"
+brew "ripgrep"
+brew "rsync"
+brew "sha2"
+brew "trash"
+brew "vagrant-completion"
+brew "wget"
+cask "adapter"
+cask "adium"
+cask "adobe-acrobat-reader"
+cask "adobe-creative-cloud"
+cask "alfred"
+cask "android-file-transfer"
+cask "appcleaner"
+cask "atom"
+cask "backblaze"
+cask "bartender"
+cask "bettertouchtool"
+cask "betterzipql"
+cask "beyond-compare"
+cask "brave"
+cask "burn"
+cask "caffeine"
+cask "cronnix"
+cask "cyberduck"
+cask "dash"
+cask "docker"
+cask "dropbox"
+cask "etcher"
+cask "evernote"
+cask "filezilla"
+cask "firefox"
+cask "firefoxdeveloperedition"
+cask "flash-player"
+cask "fluid"
+cask "fork"
+cask "google-backup-and-sync"
+cask "google-chrome"
+cask "google-chrome-canary"
+cask "handbrake"
+cask "hex-fiend"
+cask "imageoptim"
+cask "istat-menus5"
+cask "iterm2"
+cask "java"
+cask "keybase"
+cask "kindle"
+cask "lando"
+cask "launchcontrol"
+cask "libreoffice"
+cask "limechat"
+cask "liteicon"
+cask "little-snitch"
+cask "macdown"
+cask "mailplane"
+cask "microsoft-office"
+cask "muzzle"
+cask "namechanger"
+cask "opendns-updater"
+cask "opera"
+cask "path-finder"
+cask "phpstorm"
+cask "qlcolorcode"
+cask "qlimagesize"
+cask "qlmarkdown"
+cask "qlprettypatch"
+cask "qlstephen"
+cask "quicklook-csv"
+cask "quicklook-json"
+cask "remember-the-milk"
+cask "ringcentral"
+cask "secure-pipes"
+cask "sequel-pro"
+cask "shimo3"
+cask "sketch"
+cask "skitch"
+cask "skype"
+cask "slack"
+cask "sourcetree"
+cask "sqlpro-for-sqlite"
+cask "sublime-text-dev"
+cask "suspicious-package"
+cask "sync"
+cask "teamviewer"
+cask "textmate"
+cask "timer"
+cask "tower2"
+cask "transmit4"
+cask "tunnelbear"
+cask "tunnelblick"
+cask "typora"
+cask "unetbootin"
+cask "vagrant"
+cask "virtualbox"
+cask "visual-studio-code"
+cask "vlc"
+cask "vmware-fusion6"
+cask "zoomus"
+mas "1Password", id: 443987910
+mas "Be Focused", id: 973134470
+mas "Bear", id: 1091189122
+mas "Contacts Sync For Google Gmail", id: 451691288
+mas "DaisyDisk", id: 411643860
+mas "Due", id: 524373870
+mas "Fantastical 2", id: 975937182
+mas "FileShredder", id: 418094085
+mas "Gapplin", id: 768053424
+mas "HyperDock", id: 449830122
+mas "iMovie", id: 408981434
+mas "Key Codes", id: 414568915
+mas "Keynote", id: 409183694
+mas "Kiwi for Gmail", id: 986304488
+mas "Liquid | Flow", id: 569048352
+mas "LiveReload", id: 482898991
+mas "Moom", id: 419330170
+mas "MuteMyMic", id: 456362093
+mas "NotePlan", id: 1137020764
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Paste", id: 967805235
+mas "PCalc", id: 403504866
+mas "PDF Reader Pro Lite", id: 919472673
+mas "PDFScanner", id: 410968114
+mas "PocketCAS", id: 703006457
+mas "QuickLens", id: 827686482
+mas "Quiver", id: 866773894
+mas "SnippetsLab", id: 1006087419
+mas "Speedtest", id: 1153157709
+mas "Spillo", id: 873245660
+mas "StuffIt Expander", id: 405580712
+mas "TextWrangler", id: 404010395
+mas "The Unarchiver", id: 425424353
+mas "Things3", id: 904280696
+mas "Tyme2", id: 1063996724
+mas "Witch", id: 412485838
+mas "Wunderlist", id: 410628904
+mas "Xcode", id: 497799835
--- my_installer.sh
+++ my_installer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-# Useful links
-
-################################################################################
-
+# Helper function
 command_exists () {
   hash "$1" 2> /dev/null
 }
@@ -16,69 +13,34 @@ if ! command_exists ruby; then
   exit 1
 fi
 
-################################################################################
-
+# Install homebrew
 if ! command_exists brew; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# https://github.com/Homebrew/homebrew-dupes
-if ! [ `brew tap | grep homebrew/dupes` ]; then
-  brew tap homebrew/dupes
-fi
-
-# https://github.com/Homebrew/homebrew-versions
-if ! [ `brew tap | grep homebrew/versions` ]; then
-  brew tap homebrew/versions
-fi
-
-# https://github.com/Homebrew/homebrew-completions
-if ! [ `brew tap | grep homebrew/completions` ]; then
-  brew tap homebrew/completions
-fi
-
-if ! [ `brew tap | grep homebrew/php` ]; then
-  brew tap homebrew/php
-fi
-
-# https://github.com/caskroom/homebrew-cask
-if ! [ `brew tap | grep caskroom/cask` ]; then
-  brew tap caskroom/cask
-fi
-
-# https://github.com/caskroom/homebrew-versions
-if ! [ `brew tap | grep caskroom/versions` ]; then
-  brew tap caskroom/versions
-fi
-
-# https://github.com/buo/homebrew-cask-upgrade
-if ! [ `brew tap | grep buo/cask-upgrade` ]; then
-  brew tap buo/cask-upgrade
-fi
-
-#exit 0
-
-################################################################################
-
-# Install into global applications directory.
+# Install Homebrew casks into global applications directory.
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-# XQuartz, used for X11 apps.
-brew cask install xquartz
-
 # Composer - don't use Homebrew version due to PHP dependencies
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
+if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
+then
+    >&2 echo 'ERROR: Invalid installer signature'
+    rm composer-setup.php
+    exit 1
+fi
+php composer-setup.php --quiet
+rm composer-setup.php
 
 # Drush
 composer global require drush/drush
 ln -s ~/.composer/vendor/bin/drush /usr/local/bin/drush
 
 # Terminus (Pantheon)
-git clone https://github.com/pantheon-systems/terminus.git $HOME/.drush/terminus
 cd $HOME/.drush/terminus
-composer update --no-dev
-drush cc drush
+composer require pantheon-systems/terminus
 cd $CURRENTPATH;
 
 # Drush SQL Sync Pipe
@@ -86,113 +48,31 @@ cd $CURRENTPATH;
 drush dl drush_sql_sync_pipe --destination=$HOME/.drush
 drush cc drush
 
+# Homebrew taps, formulae, casks, mac app store
+brew bundle
+
 # NPM and related
-brew install node
+## Update: $ npm list -g --depth 0
 npm install -g bower
 npm install -g caniuse-cmd
+npm install -g create-react-app
+npm install -g grunt
 npm install -g gulp
+npm install -g gulp-cli
 npm install -g jshint
+npm install -g npm
+npm install -g npm-check
 npm install -g npm-check-updates
 npm install -g sass-lint
 npm install -g vtop
 npm install -g yarn
+npm install -g yo
 
-# Fish shell
-brew install fish
+# Ansible
+## https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#latest-releases-via-pip
+pip install ansible
 
-brew install ack
-brew install brew-cask-completion
-brew install git
-brew install git-extras
-brew install libdvdcss
-brew install openssl
-brew install pv
-brew install python
-brew install rsync
-brew install sqlite
-brew install wget
-
-# Vagrant and Ansible
-brew cask install virtualbox
-brew cask install vagrant
-brew install homebrew/completions/vagrant-completion
-pip install --upgrade ansible
-
-
-################################################################################
-# Apps
-
-brew cask install adapter
-brew cask install adium
-brew cask install alfred
-brew cask install android-file-transfer
-brew cask install appcleaner
-brew cask install atom
-brew cask install backblaze
-brew cask install bartender
-brew cask install bettertouchtool
-brew cask install betterzipql
-brew cask install beyond-compare
-brew cask install brackets
-brew cask install burn
-brew cask install caffeine
-brew cask install cakebrew
-brew cask install crashplan
-brew cask install dash2
-brew cask install docker
-brew cask install dropbox
-brew cask install evernote
-brew cask install filezilla
-brew cask install firefox
-brew cask install flickr-uploadr
-brew cask install fluid
-brew cask install flux
-brew cask install gas-mask
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install google-photos-backup
-brew cask install handbrake
-brew cask install hex-fiend
-# brew cask install hostbuddy # using gas-mask
-brew cask install hyper
-brew cask install integrity
-brew cask install istat-menus
-brew cask install iterm2
-brew cask install launchcontrol
-brew cask install limechat
-brew cask install liteicon
-brew cask install livereload
-brew cask install macdown
-brew cask install mailplane
-brew cask install namechanger
-brew cask install opendns-updater
-brew cask install path-finder
-brew cask install phpstorm
-brew cask install qlcolorcode
-brew cask install qlimagesize
-brew cask install qlmarkdown
-brew cask install qlprettypatch
-brew cask install qlstephen
-brew cask install quicklook-csv
-brew cask install quicklook-json
-brew cask install radiant-player
-brew cask install sequel-pro
-brew cask install shimo
-brew cask install sketch
-brew cask install skitch
-brew cask install skype
-brew cask install slack
-brew cask install sourcetree
-brew cask install sqlpro-for-sqlite
-brew cask install sublime-text
-brew cask install suspicious-package
-brew cask install sync
-brew cask install teamviewer
-brew cask install tower
-brew cask install transmit
-brew cask install tunnelbear
-# brew cask install vagrant # installed above
-# brew cask install virtualbox # installed above
-brew cask install vlc
-brew cask install vmware-fusion6
-brew cask install zoomus
+# Ruby
+curl -sSL https://get.rvm.io | bash -s stable -- --ignore-dotfiles
+# rvm install <ruby version>
+rvm install 2.5.1
