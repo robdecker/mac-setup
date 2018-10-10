3d90a5f3eb36850483fce5b20e797ec745775310
Rob Decker <rob@deckerdev.com>
2018-10-10 16:58:18 -0700

    Switch composer wget to curl

--- my_installer.sh
+++ my_installer.sh
@@ -22,7 +22,7 @@ fi
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # Composer - don't use Homebrew version due to PHP dependencies
-EXPECTED_SIGNATURE="$(wget -q -O - https://composer.github.io/installer.sig)"
+EXPECTED_SIGNATURE="$(curl -s https://composer.github.io/installer.sig)"
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_SIGNATURE="$(php -r "echo hash_file('SHA384', 'composer-setup.php');")"
 if [ "$EXPECTED_SIGNATURE" != "$ACTUAL_SIGNATURE" ]
